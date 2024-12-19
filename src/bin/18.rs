7da6952776de6c69928fc88b0c79b384cb86dfe2
John Kryspin <johnkryspin@gmail.com>
2024-12-18 21:41:14 -0500

    boom

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -4,103 +4,83 @@ advent_of_code::solution!(18);
 
 pub fn part_one(input: &str) -> Option<u32> {
     let res = solve(input, true, 71, 71, 1024);
-    return Some(res as u32);
+    Some(res as u32)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-
-        let mut grid:Vec<Vec<char>> = vec![vec!['.'; 71]; 71];
-        let mut positions:Vec<(usize, usize, usize)> = Vec::new();
-        input.lines().enumerate().for_each(|(i,s)|{
-            let (left, right) = s.split_once(",").unwrap();
-            positions.push((left.parse().unwrap(), right.parse().unwrap(), i));
-        });
-        println!("{:?}", positions);
-
+pub fn part_two(input: &str) -> Option<String> {
+    let grid: Vec<Vec<char>> = vec![vec!['.'; 71]; 71];
+    let mut positions: Vec<(usize, usize, usize)> = Vec::new();
+    input.lines().enumerate().for_each(|(i, s)| {
+        let (left, right) = s.split_once(',').unwrap();
+        positions.push((left.parse().unwrap(), right.parse().unwrap(), i));
+    });
 
     let mut i = 1024usize;
     loop {
-        let mut temp_grid= grid.clone();
+        let mut temp_grid = grid.clone();
         let mut last = None;
-        for (x,y,curr_i) in positions.iter() {
-            if (curr_i >= &i) {
+        for (x, y, curr_i) in positions.iter() {
+            if curr_i >= &i {
                 break;
             }
-            last = Some((x,y));
+            last = Some((x, y));
             temp_grid[*y][*x] = '#';
-        };
+        }
         let res = bfs(&temp_grid, (0, 0), (71 - 1, 71 - 1));
         if res == 0 {
-            // print last
-            println!("{:?}", last);
-            return Some(i as u32);
+            return Some(format!("{},{}", last.unwrap().0, last.unwrap().1));
         }
         i += 1;
     }
 }
 
-fn solve(input:&str, part_one:bool, width:usize, height:usize, limit: usize) -> i32 {
-    let mut grid:Vec<Vec<char>> = vec![vec!['.'; width]; height];
-    let mut positions:Vec<(usize, usize, usize)> = Vec::new();
-    input.lines().enumerate().for_each(|(i,s)|{
-        let (left, right) = s.split_once(",").unwrap();
+fn solve(input: &str, part_one: bool, width: usize, height: usize, limit: usize) -> i32 {
+    let mut grid: Vec<Vec<char>> = vec![vec!['.'; width]; height];
+    let mut positions: Vec<(usize, usize, usize)> = Vec::new();
+    input.lines().enumerate().for_each(|(i, s)| {
+        let (left, right) = s.split_once(',').unwrap();
         positions.push((left.parse().unwrap(), right.parse().unwrap(), i));
     });
-    println!("{:?}", positions);
 
-    for (x,y,i) in positions.iter() {
-        if part_one{
-            if (i >= &limit) {
-                break;
-            }
+    for (x, y, i) in positions.iter() {
+        if part_one && i >= &limit {
+            break;
         }
-        // if out of bounds
-        println!("{} {} {}", x, y, i);
         grid[*y][*x] = '#';
-    };
-    // print grid
-    grid.iter().for_each(|row|{
-        row.iter().for_each(|c|{
-            print!("{}", c);
-        });
-        println!();
-    });
+    }
 
-    let res = bfs(&grid, (0,0), (width-1, height-1));
-    println!("{}", res);
-    return res;
+    
+    bfs(&grid, (0, 0), (width - 1, height - 1))
 }
 
-fn bfs(grid: &Vec<Vec<char>>, start: (usize,usize), end: (usize,usize)) -> i32 {
+fn bfs(grid: &Vec<Vec<char>>, start: (usize, usize), end: (usize, usize)) -> i32 {
     let mut q = VecDeque::new();
     let mut visited = vec![vec![false; grid[0].len()]; grid.len()];
     q.push_back((start, 0));
-    while let Some((curr, len)) = q.pop_front(){
+    while let Some((curr, len)) = q.pop_front() {
         if visited[curr.1][curr.0] {
             continue;
         }
         visited[curr.1][curr.0] = true;
-        if curr == end{
+        if curr == end {
             return len;
         }
 
-        let dirs = vec![(0,1), (0,-1), (1,0), (-1,0)];
-        for (dx,dy)in dirs {
-            let x = curr.0 as i32 + dx as i32;
-            let y = curr.1 as i32 + dy as i32;
+        let dirs = vec![(0, 1), (0, -1), (1, 0), (-1, 0)];
+        for (dx, dy) in dirs {
+            let x = curr.0 as i32 + dx;
+            let y = curr.1 as i32 + dy;
             if x < 0 || y < 0 || x >= grid[0].len() as i32 || y >= grid.len() as i32 {
                 continue;
             }
-            if grid[y as usize][x as usize] == '#'{
+            if grid[y as usize][x as usize] == '#' {
                 continue;
             }
-            q.push_back(((x as usize, y as usize), len+1));
+            q.push_back(((x as usize, y as usize), len + 1));
         }
-
     }
     0
 }
-
 
 #[cfg(test)]
 mod tests {
